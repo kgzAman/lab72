e932981a96384291658db9ff719143b22d40ce47
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-07-05 17:57:15 +0600

    add service

--- src/main/java/com/edu/aman/chat/sercice/MassageService.java
+++ src/main/java/com/edu/aman/chat/sercice/MassageService.java
@@ -10,14 +10,14 @@ import java.util.List;
 
 @Service
 @Data
-//public class MassageService {
-//    private final MassageRepository massageRepository;
-//
-//    public List<Massage> getAll() {
-//        List<Massage> massages= massageRepository.findAll();
-//        if(massages==null){
-//            List<Massage> massages1 = new ArrayList<>();
-//            return massages1;
-//        }else return massages;
-//    }
+public class MassageService {
+    private final MassageRepository massageRepository;
+
+    public List<Massage> getAll() {
+        List<Massage> massages= massageRepository.findAll();
+        if(massages==null){
+            List<Massage> massages1 = new ArrayList<>();
+            return massages1;
+        }else return massages;
+    }
 }
